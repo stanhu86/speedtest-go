d3c6e0e3fe17bd634993815c050ab448a6d1043a
Stanley Hu <stanthetiger@gmail.com>
2025-09-24 00:02:20 +0800

    set chunkSize to 10mb and remove xhr abort

--- web/assets/speedtest_worker.js
+++ web/assets/speedtest_worker.js
@@ -56,8 +56,8 @@ var settings = {
 	xhr_multistreamDelay: 300, //how much concurrent requests should be delayed
 	xhr_ignoreErrors: 1, // 0=fail on errors, 1=attempt to restart a stream if it fails, 2=ignore all errors
 	xhr_dlUseBlob: false, // if set to true, it reduces ram usage but uses the hard drive (useful with large garbagePhp_chunkSize and/or high xhr_dlMultistream)
-	xhr_ul_blob_megabytes: 20, //size in megabytes of the upload blobs sent in the upload test (forced to 4 on chrome mobile)
-	garbagePhp_chunkSize: 100, // size of chunks sent by garbage.php (can be different if enable_quirks is active)
+	xhr_ul_blob_megabytes: 10, //size in megabytes of the upload blobs sent in the upload test (forced to 4 on chrome mobile)
+	garbagePhp_chunkSize: 10, // size of chunks sent by garbage.php (can be different if enable_quirks is active)
 	enable_quirks: true, // enable quirks for specific browsers. currently it overrides settings to optimize for specific browsers, unless they are already being overridden with the start command
 	ping_allowPerformanceApi: true, // if enabled, the ping test will attempt to calculate the ping more precisely using the Performance API. Currently works perfectly in Chrome, badly in Edge, and not at all in Firefox. If Performance API is not supported or the result is obviously wrong, a fallback is provided.
 	overheadCompensationFactor: 1.06, //can be changed to compensatie for transport overhead. (see doc.md for some other values)
@@ -272,16 +272,9 @@ function clearRequests() {
 				xhr[i].onprogress = null;
 				xhr[i].onload = null;
 				xhr[i].onerror = null;
-			} catch (e) {}
-			try {
 				xhr[i].upload.onprogress = null;
 				xhr[i].upload.onload = null;
 				xhr[i].upload.onerror = null;
-			} catch (e) {}
-			try {
-				xhr[i].abort();
-			} catch (e) {}
-			try {
 				delete xhr[i];
 			} catch (e) {}
 		}
